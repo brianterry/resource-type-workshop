d00b8c2c9c3cfe41f4efe7a7c37078eeb1d94112
Brian Terry <brianter@amazon.com>
2021-12-14 08:12:34 -0800

    update script

--- resize.sh
+++ resize.sh
@@ -59,3 +59,9 @@ else
     sudo resize2fs /dev/nvme0n1p1
   fi
 fi
+
+# install mvn
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+sudo yum install -y apache-maven
+mvn --version
